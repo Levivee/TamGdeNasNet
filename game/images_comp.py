f487cda41ca41633a37e6769dafc28517e4bbb03
LeviVee <violetta729@list.ru>
2020-12-13 18:34:02 +0300

    ~~

--- game/images_comp.py
+++ game/images_comp.py
@@ -1,4 +1,4 @@
 
-for i in range(200, 230):
+for i in range(200, 256):
     print('image m{}{}{}'.format(i // 100, i % 100 // 10, i % 10) + '="images/monika/mon{}{}{}'.format(i // 100, i % 100 // 10, i % 10) + '.png"')
 
